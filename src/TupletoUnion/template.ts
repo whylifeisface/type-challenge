c9ab366deddb4334e49bc344a411fa9292c2f0a6
soup <o826y396@163.com>
2023-05-21 23:30:31 +0800

    tupletounion

---
+++ src/TupletoUnion/template.ts
@@ -0,0 +1,2 @@
+type TupleToUnion<T extends unknown[]> = T[number]
+//啊 数组这样遍历? 结果对了?
--- src/deeo-readonly/template.ts
+++ src/deeo-readonly/template.ts
@@ -1,1 +1,4 @@
-type DeepReadonly<T> = any
+type DeepReadonly<T> = {
+    readonly [P in keyof T]: T[P] extends (...args: unknown[]) => void ? T[P] : T[P] extends Object ? DeepReadonly<T[P]> : T[P]
+}
+ //缺少 (...args:unknown[])=>void 判断
